c65bb64640faa717c2138a4e5883fd230a20eb87
Alan Pecenkovic <alanquad@yahoo.com>
2023-02-17 11:01:14 +0100

    rendering error pages

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -24,32 +24,58 @@ const handleJTWError = () => new AppError("Invalid token. Please log in again!",
 const handleJTWExpiredError = () =>
   new AppError("Your token has expired! Please log in again.", 401);
 
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
+const sendErrorDev = (err, req, res) => {
+  // API
+  if (req.originalUrl.startsWith("/api")) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  }
+  // Rendered website
+  console.error("ERROR ☢️", err);
+  return res.status(err.statusCode).render("error", {
+    title: "Something went wrong!",
+    msg: err.message,
   });
 };
 
-const sendErrorProd = (err, res) => {
-  // Operational, trusted error: send message to client
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
+const sendErrorProd = (err, req, res) => {
+  // API
+  if (req.originalUrl.startsWith("/api")) {
+    // Operational, trusted error: send message to client
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
     // Programming or other unknown error: don't leak error details
-  } else {
     // 1. Log error
     console.error("ERROR ☢️", err);
     // 2. Send generic message
-    res.status(500).json({
+    return res.status(500).json({
       status: "error",
       message: "Something went very wrong!",
     });
   }
+  // Rendered website
+  if (err.isOperational) {
+    return res.status(err.statusCode).render("error", {
+      title: "Something went wrong!",
+      msg: err.message,
+    });
+  }
+  // Programming or other unknown error: don't leak error details
+  // 1. Log error
+  console.error("ERROR ☢️", err);
+  // 2. Send generic message
+  return res.status(err.statusCode).render("error", {
+    title: "Something went wrong!",
+    msg: "Please try again later!",
+  });
 };
 
 module.exports = (err, req, res, next) => {
@@ -59,7 +85,7 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || "error";
 
   if (process.env.NODE_ENV === "development") {
-    sendErrorDev(err, res);
+    sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === "production") {
     // let error = { ...err };
     // In later versions of mongoose the name property of CastError is not an enumerable
@@ -73,6 +99,6 @@ module.exports = (err, req, res, next) => {
     if (error.name === "JsonWebTokenError") error = handleJTWError();
     if (error.name === "TokenExpiredError") error = handleJTWExpiredError();
 
-    sendErrorProd(error, res);
+    sendErrorProd(error, req, res);
   }
 };
